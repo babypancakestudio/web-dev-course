b31f8871292440abbd46aae06890c4dc6be05e21
Shane Eller <eller.shane.gamedev@gmail.com>
2025-11-26 16:38:09 -0500

    Stop tracking .idea and media directories

---
+++ 4 Scripts/jQuery-Ex.js
@@ -0,0 +1,16 @@
+﻿// $("#circle").on('click',function(){
+//
+//     alert("circle clicked!");
+// });
+//
+// $(".square").on('click',function(){
+//     alert("square clicked!");
+// })
+
+//document.getElementById("circle").style.backgroundColor = "green";
+
+//$("#circle").style.backgroundColor = "orange";
+
+$("div").on('click', function () {
+    $(this).fadeOut()
+})

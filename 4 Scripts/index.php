62d6a125266c4fe4ca6c59c891c6387becab36d5
Shane Eller <eller.shane.gamedev@gmail.com>
2025-12-02 14:23:14 -0500

    more php course work

--- 4 Scripts/index.php
+++ 4 Scripts/index.php
@@ -1,3 +1,9 @@
 ﻿<?php
-    echo "Hello World!";
+    $name = "John";
+
+    define("MESSAGE", "Hello friends!");
+
+    echo "<p>My name is $name</p>";
+
+    echo "<p>I'd like to say" . MESSAGE . "</p>";
 ?>
---
+++ 5-PHP/5-1-PHP/index.php
@@ -0,0 +1,30 @@
+﻿<?php
+    if($_POST){
+        $family = array("Henry", "Ellen", "Jack", "Shane");
+
+        $isKnown = false;
+
+        foreach ($family as $value) {
+            if($value == $_POST["name"]){
+                $isKnown = true;
+                break;
+            }
+        }
+        if($isKnown){
+            echo "hi there, ".$_POST["name"].'!';
+        }
+        else{
+            echo "Sorry, I don't know you.";
+        }
+
+    }
+
+?>
+
+<form method="post">
+    <p>What is your name?</p>
+
+    <p><input type="text" name="name" autocomplete="off"></p>
+    <p><input type="submit" value="Submit"></p>
+
+</form>

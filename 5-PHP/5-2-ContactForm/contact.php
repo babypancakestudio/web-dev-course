b0bd5cfeca0962342b573176a89ba615d3280c10
Shane Eller <eller.shane.gamedev@gmail.com>
2025-12-05 11:15:44 -0500

    updated contact form. consolidated css stylesheets into contactstyle. not using javascript, file deleted.

--- 5-PHP/5-2-ContactForm/contact.php
+++ 5-PHP/5-2-ContactForm/contact.php
@@ -1,23 +1,30 @@
 ﻿<?php include 'header.php';?>
-<style><?php include 'meyerweb-reset.css'; ?></style>
 <style><?php include 'contactstyle.css'; ?></style>
 
 
+<div class="contact-container">
+    <h2> Contact Us</h2>
 
-<h2 id="contact"> Contact Us</h2>
-<form method="post" action="send_form.php">
-    <label>First Name</label>
-    <input type="text" name="firstname" placeholder="First Name"><br>
-    <label>Last Name</label>
-    <input type="text" name="lastname" placeholder="Last Name"><br>
-    <label>Email</label>
-    <input type="email" name="email" placeholder="Email Address"><br>
-    <label>Subject</label>
-    <input type="text" name="subject" placeholder="Subject"><br>
-    <label>Message</label>
-    <textarea name="What would you like to ask" cols="30" rows="10"></textarea><br>
-    <input type="submit" value="Submit">
-</form>
+    <form method="post" action="send_form.php" class="contact-form" autocomplete="off">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="What's your name?" required>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Email Address" required>
+
+        <label for="message">Message</label>
+        <textarea id="message" name="message" placeholder="What's up?" required></textarea><br>
+
+        <!-- Honeypot field (hidden from users) -->
+        <div class="hidden-field">
+            <label>Leave this field empty</label>
+            <input type="text" name="website">
+        </div>
+
+        <button type="submit">Send Message</button>
+    </form>
+</div>
+
 
 <?php include 'footer.php';?>
 
